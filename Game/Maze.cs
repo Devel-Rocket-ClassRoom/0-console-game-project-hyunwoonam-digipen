104e842a9c16038e36ddd23392306693532f41bf
DESKTOP-NJGSL36\ok623 <hyunwoo.nam@digipen.edu>
2026-03-19 14:43:56 +0900

    작성중

---
+++ Game/Maze.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MazeGenerator
+{
+    private int width;
+    private int height;
+
+    private int cellWidth;
+    private int cellHeight;
+
+    private int[,] map;
+    private Random rand = new Random();
+
+    MazeGenerator(int w, int h)
+    {
+        width = w % 2 == 0 ? w + 1 : w;
+        height = h % 2 == 0 ? h + 1 : h;
+
+        cellHeight = (height - 1) / 2;
+        cellWidth = (width - 1) / 2;
+
+        map = new int[width, height];
+    }
+
+    public void Generate()
+    {
+        //전체 그리드 벽으로
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                map[i, j] = 1;
+            }
+        }
+
+        int[] currRow = new int[cellWidth];
+
+        //첫 셀 행의 고유 집합 번호 부여
+        for (int i = 0; i < cellWidth; i++)
+        {
+            currRow[i] = i+1;
+        }
+
+        int nextSetId = cellWidth;
+
+        for (int y = 0; y < cellHeight; y++)
+        {
+            int currCol = y * 2 + 1;
+
+            //방파기
+            for (int x = 0; x < cellWidth; x++)
+            {
+                map[currCol, x * 2 + 1] = 0;
+            }
+        }
+
+        for (int y = 0; y < cellHeight; y++)
+        {
+            int currCol = y * 2 + 1;
+
+            //수평 연결
+            for (int x = 0; x < cellWidth - 1; x++)
+            {
+                if (currRow[x] == currRow[x + 1])
+                {
+                    continue;
+                }
+
+                if (rand.Next(2) == 0)
+                {
+                    map[currCol, x * 2 + 2] = 0;
+
+                    int oldSet = currRow[x + 1];
+                    int newSet = currRow[x];
+
+                    for (int i = 0; i < cellWidth; i++)
+                    {
+                        if (currRow[i] == oldSet)
+                        {
+                            currRow[i] = newSet;
+                        }
+                    }
+                }
+            }
+
+            //수직 연결
+            if(y < cellHeight -1)
+            {
+
+            }
+
+        }
+
+        
+
+    }
+
+
+}
